6782089ec36ec889cd474a17533dc9783a075f7b
Igor Claudino de França Costa <igor.claudino.ic@gmail.com>
2021-05-20 09:46:01 -0300

    Adiciona paginação e DTO para Event

--- src/main/java/br/ufc/crateus/npds/events/service/EventService.java
+++ src/main/java/br/ufc/crateus/npds/events/service/EventService.java
@@ -5,6 +5,7 @@ import br.ufc.crateus.npds.events.exception.RecordNotFoundException;
 import br.ufc.crateus.npds.events.models.Event;
 import br.ufc.crateus.npds.events.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,8 +19,8 @@ public class EventService {
     private EventRepository eventRepository;
 
 
-    public List<Event> getAll(){
-        List<Event> events = (List<Event>) eventRepository.findAll();
+    public List<Event> getAll(int pageNumber, int pageSize){
+        List<Event> events =  eventRepository.findAll(PageRequest.of(pageNumber, pageSize));
         return events;
     }
 
@@ -36,8 +37,10 @@ public class EventService {
         else throw new RecordNotFoundException();
     }
 
-    public List<Event> getByQuery(String query) {
-        List<Event> events = eventRepository.findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCaseOrLocalizationContainingIgnoreCaseOrOrganizerContainingIgnoreCase(query, query, query, query);
+    public List<Event> getByQuery(String query, int pageNumber, int pageSize ) {
+        List<Event> events =
+                eventRepository.
+                findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCaseOrLocalizationContainingIgnoreCaseOrOrganizerContainingIgnoreCase(query, query, query, query, PageRequest.of(pageNumber, pageSize));
         return events;
     }
 }
---
+++ src/main/java/br/ufc/crateus/npds/events/service/ScheduleService.java
@@ -0,0 +1,41 @@
+package br.ufc.crateus.npds.events.service;
+
+import br.ufc.crateus.npds.events.exception.InvalidDateException;
+import br.ufc.crateus.npds.events.exception.InvalidEndDateException;
+import br.ufc.crateus.npds.events.exception.RecordNotFoundException;
+import br.ufc.crateus.npds.events.models.Event;
+import br.ufc.crateus.npds.events.models.Schedule;
+import br.ufc.crateus.npds.events.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScheduleService {
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Autowired
+    private EventService eventService;
+
+    public Schedule insert(Schedule schedule, Integer eventId) throws RecordNotFoundException, InvalidEndDateException, InvalidDateException {
+        Event event = eventService.getById(eventId);
+        schedule.setEvent(event);
+        if(schedule.getEndDate().before(schedule.getBeginDate())){
+            throw new InvalidEndDateException();
+        }
+        if((schedule.getBeginDate().after(event.getEndDate()) || schedule.getBeginDate().before(event.getBeginDate())) || (schedule.getEndDate().after(event.getEndDate()) || schedule.getEndDate().before(event.getBeginDate()))){
+            throw new InvalidDateException();
+        }
+        return scheduleRepository.save(schedule);
+    }
+
+    public List<Schedule> getByEvent(Integer eventId, int pageNumber, int pageSize) throws RecordNotFoundException {
+        Event event = eventService.getById(eventId);
+
+        return scheduleRepository.findByEvent(event, PageRequest.of(pageNumber, pageSize));
+
+    }
+}
